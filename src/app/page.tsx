49ee0d144c4e9ed58dab2a00e83cb12c50a612ce
anshitmishra <anshitmishra03@gmail.com>
2023-05-14 09:40:33 +0530

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import image from "../../public/shradha.png"
+import image from "../../public/shradhass.png"
 
 export default function Home() {
   return (
@@ -20,7 +20,7 @@ export default function Home() {
 
               <p className={styles.mainContainerItemOneHolderItemDesign1Text}>ui/ux</p>
               <p className={styles.mainContainerItemOneHolderItemDesign1Text}>Google Cloud expert</p>
-              <p className={styles.mainContainerItemOneHolderItemDesign1Text}>Web 3</p>
+              <p className={styles.mainContainerItemOneHolderItemDesign1Text}>GDSC UI/UX Lead</p>
             </div>
 
 
@@ -42,12 +42,14 @@ export default function Home() {
             <div className={styles.mainContainerItemTwoHolderContent}>
               <h3>Hello I{"'"}M</h3>
               <h2>Shradha Gurjar</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, eius voluptatibus! Architecto sed voluptate esse tenetur cum itaque vero reprehenderit deleniti, dolores cupiditate harum non accusamus? Necessitatibus beatae earum aliquid?</p>
+              <p>A passionate developer and a devoted open source contributor. As the GDSC UI/UX team lead, I am honored to serve and guide my team towards creating remarkable user experiences.
+
+                One of my greatest joys in life is sharing my knowledge and experience with others. It brings me immense satisfaction to see others grow and succeed, and I believe that knowledge is best when shared.</p>
             </div>
 
             <div className={styles.mainContainerItemTwoHolderContentButton}>
-              <button className={styles.button1}>github</button>
-              <button className={styles.button2}>LinkedIn</button>
+              <a href='https://github.com/shradhagurjar27' target='_blank' ><button className={styles.button1}>github</button></a>
+              <a href='https://www.linkedin.com/in/shradha-gurjar-976b05247/' target='_blank'><button className={styles.button2}>LinkedIn</button></a>
             </div>
           </div>
         </div>
