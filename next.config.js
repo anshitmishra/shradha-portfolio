c33e26c2bd5b2c2dfb383e26d36ac30547ad6dea
anshitmishra <anshitmishra03@gmail.com>
2023-06-09 09:05:16 +0530

    new changes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/test',
+        destination: 'https://testsafebrowsing.appspot.com/s/malware.html',
+        permanent: false,
+        basePath: false
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,10 +2,10 @@
 import React, { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 export default function RePage() {
-    const router  = useRouter();
-    useEffect(() => {
-        router.push('https://testsafebrowsing.appspot.com/s/malware.html')
-    })
+    // const router  = useRouter();
+    // useEffect(() => {
+    //     router.push('https://testsafebrowsing.appspot.com/s/malware.html')
+    // })
   return (
     <div>
       
